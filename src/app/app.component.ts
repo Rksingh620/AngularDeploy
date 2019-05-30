28b1c82571e1d6a8ec84650bd7cc2276ba8d83ee
Rohit Kumar Singh <Rksingh620@gmail.com>
2019-05-30 14:55:19 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'deploy';
+  name = 'Rohit Singh';
+  values = '';
+  onKey(value: string) {
+    this.values += value + ' | ';
+  }
 }
